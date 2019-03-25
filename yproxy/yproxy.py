159bd3dfd4ec717c65cc4194d5879a47ecd3eeac
yang <mightyang@hotmail.com>
2019-03-26 00:14:22 +0800

    initialize misc repo

---
+++ yproxy/yproxy.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : yproxy.py
+# Author            : yang <mightyang@hotmail.com>
+# Date              : 23.03.2019
+# Last Modified Date: 23.03.2019
+# Last Modified By  : yang <mightyang@hotmail.com>
+
+
+import socket
+import threading
+
+class yserver():
+    # 初始化
+
+    # 监听端口
+
+    # 随机分配连接端口
+
+    # 
+
+
+class client():
+    pass
